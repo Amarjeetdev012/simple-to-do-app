878b099d3669f513c242da6b7fbe747a3722fbe1
unknown <amarjeet@baruzotech.com>
2022-11-24 10:30:52 +0530

    add nodeapp.js in package.json

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-var express = require('express');
-var todoController = require('./controllers/todoController');
+var express = require("express");
+var todoController = require("./controllers/todoController");
 var app = express();
 
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({ extended: false }));
 //set up template engine
-app.set('view engine','ejs');
+app.set("view engine", "ejs");
 
 //static files
-app.use(express.static('./public'));
+app.use(express.static("./public"));
 
 //fire controlers
 todoController(app);
 
 //listen to port
 app.listen(3000);
-console.log('you are listening to port 3000');
+console.log("you are listening to port 3000");
